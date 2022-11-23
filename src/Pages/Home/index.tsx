cf41a6c028372d0d43fc43bedbb6789398e6b3d0
Bernardo2459 <bernardodeoliveirasouza9@gmail.com>
2022-11-23 11:17:19 -0300

    atualizações

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState, } from 'react';
+import AxiosInstance from '../../Api/AxiosInstance';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  
+} from 'react-native';
+
+const Home= () =>{
+    return(
+        <>
+        </>
+    )
+}
+
+export default Home
---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect, useState, } from 'react';
+import AxiosInstance from '../../Api/AxiosInstance';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+  Alert
+} from 'react-native';
+
+const Login = () =>{
+    return(
+        <>
+        </>
+    )
+}
+
+export default Login
